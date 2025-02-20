da914e7bd7d289f327ad25b0c60d97955c7c8f14
Jaewoo Kang <jaewookang17@gmail.com>
2025-02-20 16:32:42 +0100

    what netlify said

--- src/components/FaceModel.tsx
+++ src/components/FaceModel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useImperativeHandle, forwardRef } from 'react';
+import { useEffect, useRef, useImperativeHandle, forwardRef } from 'react';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
